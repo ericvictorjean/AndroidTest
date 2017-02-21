1e395d72d18e63d30ee105bc6b793c69df484e01
ERIC ARAUJO GIL <earaujgi7.alumnes@ub052229.mat.ub.edu>
2017-02-21 08:49:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/earaujgi7alumnes/myapplication2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.earaujgi7alumnes.myapplication2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button createButton = (Button)findViewById(R.id.create);
+        createButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                // create an Intent using the current Activity
+                // and the Class to be created
+                Intent i = new Intent(MainActivity.this,Main2Activity.class);
+                // pass the Intent to the Activity,
+                // using the specified action defined in StartPage
+                startActivity(i);
+            }
+        });
+    }
+}
